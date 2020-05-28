ccc77ef53324ed94a7b41030731933e4a7bee545
samick17 <boneache@gmail.com>
2020-05-28 15:46:15 +0800

    1. Updated version & modules

--- src/command/index.js
+++ src/command/index.js
@@ -1,6 +1,7 @@
 export { default as BaseCommand } from './BaseCommand';
 export { default as MacroCommand } from './MacroCommand';
 export { default as CommandManager } from './CommandManager';
+export { Events as CommandManagerEvents } from './CommandManager';
 
 // export default {
 // 	BaseCommand,
--- src/mode/index.js
+++ src/mode/index.js
@@ -1,5 +1,6 @@
 export { default as AbstractMode } from './AbstractMode';
 export { default as AbstractModeController } from './AbstractModeController';
+export { Events as ModeControllerEvents } from './AbstractModeController';
 
 // export default {
 // 	AbstractMode,
